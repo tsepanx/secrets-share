5d5596f98f20974614884ef149b23031d124b5fb
tsepanx <77626319+tsepanx@users.noreply.github.com>
2021-12-03 15:53:36 +0300

    Message: is_encrypted field

--- secrets_share/models.py
+++ secrets_share/models.py
@@ -12,27 +12,38 @@ class User(AbstractUser):
 
 
 class Message(models.Model):
+    HASH_ID_SIZE = 8
+
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=1000)
     submit_date = models.DateTimeField('Date created', auto_now_add=True)
-    # is_encrypted = models.BooleanField(default=True)
+    is_encrypted = models.BooleanField()
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None, **kwargs):
         password = kwargs.get('password', None)
+
         if password:
             self.text = encrypt(self.text, password)
+            self.is_encrypted = True
+        else:
+            self.is_encrypted = False
 
         super().save(force_insert, force_update, using, update_fields)
 
     def get_hash_id(self):
+        """
+        Get a hash string from Model primary key, and limit its length with Message.HASH_ID_SIZE
+        """
+
         id_string = str(self.id)
-        # salt = 'my_salt'
         salt = settings.SECRET_KEY
 
         h = hashlib.sha256(id_string.encode())
         h.update(salt.encode())
 
-        return h.hexdigest()
+        hash_string = h.hexdigest()
+
+        return hash_string[:Message.HASH_ID_SIZE + 1]
 
     def get_decrypted_text(self, password):
         return decrypt(self.text, password)
